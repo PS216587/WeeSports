d880d18b3d8a6b476eee25d4e9adc409c4720b0d
Danyelllll <danyel.meijers@gmail.com>
2022-06-01 09:00:39 +0200

    route fixed

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::middleware([
 ])->group(function () {
     Route::get('/formule_1', function () {
         return view('formule_1');
-    })->name('Formule 1');
+    })->name('formule_1');
     Route::get('/home', function () {
         return view('home');
     })->name('home');
